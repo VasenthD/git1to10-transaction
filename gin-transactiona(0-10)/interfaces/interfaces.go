25321833933f7a8c6561bc3da6003f97339e11e1
VasenthD <vasenth01@gmail.com>
2023-09-21 15:23:52 +0530

    deleted

--- gin-transactiona(0-10)/interfaces/interfaces.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "transaction1to10/models"
-
-type Customer interface {
-	CreateCustomer(models.Customer) (models.DBresponse, error)
-}
--- gin-transactiona(0-10)/interfaces/transaction.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "transaction1to10/models"
-
-type ITransaction interface{
-	MakeTransaction(transactiong models.Transaction)(string,error)
-}
